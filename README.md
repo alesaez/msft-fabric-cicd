7734509d3b57938dc31018bda73afe7fae1e88b4
Alejandra Guillen <4201052+aleguillen@users.noreply.github.com>
2025-01-15 22:08:21 -0600

    Initial commit to execute Opt 2 git based deployments usign GH workflows

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# msft-fabric-cicd
-Microsoft Fabric CI/CD process
+# Fabric CICD - Option 2 - Git-based deployment using GitHub Workflows 
+
+This repository uses the [Fabric Item APIs](https://learn.microsoft.com/en-us/rest/api/fabric/core/items/update-item) to publish and unpublish Fabric Items to a given workspace ID.
+
+This script and GitHub Action a package of a Microsoft Fabric team Python named `fabric-cicd`. You can also use this locally and extend to other item types. Currently this repo only supports Environments, Data Pipelines and Notebooks.
+
+Micorosft GitHub repo is here: [microsoft/fabric-cicd](https://github.com/microsoft/fabric-cicd)
+
+The source code of the package located in this directory: `https://github.com/microsoft/fabric-cicd/tree/main/src/fabric_cicd` is copied in this repo here `/demo/fabric_cicd` to be imported by `main.py` script.
+
+The `main.py` script is launched by the GitHub Action.
+
+## Workspace Items Directory
+
+The workspace items are located in the `workspace` directory. This directory contains the following structure:
+
+```
+workspace
+    ├───Hello World.Notebook
+    ├───Run Hello World.DataPipeline
+    └───World.Environment
+        └───Setting
+```
+
+## Running the Script Locally
+
+If necessary, you can run the script locally by executing `main.py`:
+
+```bash
+python main.py
+```
+
+There are Fabric items that can't use Service Principal or Managed Identity Authentication, for example, Data Pipelines can't use Service Principal, so you can run main.py locally and use your user credentials. 
+
+## GitHub Actions Workflow
+
+To automate the deployment of the Python code, you can use the GitHub Actions workflow in this repo. Make sure to add `AZURE_TENANT_ID`, `AZURE_CLIENT_ID` and `AZURE_CLIENT_SECRET` as [secrets](https://docs.github.com/en/actions/security-for-github-actions/security-guides/using-secrets-in-github-actions) in your GitHub repository settings.
---
+++ main.py
@@ -0,0 +1,57 @@
+
+"""
+Example of authenticating with SPN + Secret
+Can be expanded to retrieve values from Key Vault or other sources
+If using Current User Credentials, ensure you have logged in using 'az login --use-device-code --allow-no-subscriptions' and have the required permissions to the Fabric Workspace
+
+Pre-requisites:
+pip install -r requirements.txt
+pip install fabric-cicd
+
+"""
+
+import os
+
+# Setting parameters
+environment = ""
+repository_directory = os.path.join(os.path.dirname(__file__), "workspace") # Path to the workspace directory
+
+from azure.identity import ClientSecretCredential, DefaultAzureCredential
+from fabric_cicd import FabricWorkspace, publish_all_items, unpublish_all_orphan_items
+
+# Authentication to Azure
+if os.getenv("GITHUB_ACTIONS") == "true": # Check if running in GitHub Actions to Use Service Principal Authentication otherwise use DefaultAzureCredential
+    print("Running from GitHub Actions - Using Service Principal Authentication")
+    workspace_id = os.getenv("FABRIC_WORKSPACE_ID") 
+    client_id = os.getenv("AZURE_CLIENT_ID") # Secret defined in GitHub - Service Principal ID
+    client_secret = os.getenv("AZURE_CLIENT_SECRET") # Secret defined in GitHub - Service Principal Secret
+    tenant_id = os.getenv("AZURE_TENANT_ID") # Secret defined in GitHub - Tenant ID
+    if not all([client_id, client_secret, tenant_id]):
+        raise ValueError("Missing Azure credentials")
+
+    token_credential = ClientSecretCredential(
+        client_id=client_id, client_secret=client_secret, tenant_id=tenant_id
+    )
+    item_type_in_scope = ["Notebook", "Environment"]
+else:
+    print("Running from Local - Using Current User Credentials")
+    token_credential = DefaultAzureCredential() 
+    workspace_id = input("Enter your Workspace ID: ") # Fabric Workspace ID
+    item_type_in_scope = ["Notebook"]
+
+
+print (f"Using Workspace ID: {workspace_id}")
+print (f"Using Repository Directory: {repository_directory}")
+print (f"Using Item Types: {item_type_in_scope}")
+# Initialize the FabricWorkspace object with the required parameters
+target_workspace = FabricWorkspace(
+    workspace_id=workspace_id,
+    repository_directory=repository_directory,
+    item_type_in_scope=item_type_in_scope,
+)
+
+# Publish all items defined in item_type_in_scope
+publish_all_items(target_workspace)
+
+# Unpublish all items defined in item_type_in_scope not found in repository
+unpublish_all_orphan_items(target_workspace)
